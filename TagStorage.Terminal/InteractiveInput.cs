f5b57cafd66afe92cf7a6fa6826032005a58cac5
Terochi <mates.cernicky@gmail.com>
2025-05-27 22:38:50 +0200

    Added interactive console input for responsive search

---
+++ TagStorage.Terminal/InteractiveInput.cs
@@ -0,0 +1,114 @@
+﻿using System.Text;
+
+namespace TagStorage.Terminal;
+
+public class InteractiveInput(Func<string, IEnumerable<string>> onChange, string prompt = "> ")
+{
+	private const int max_display_lines = 10;
+
+	private readonly StringBuilder inputBuffer = new StringBuilder();
+	private int cursorLeft = 0;
+
+	private IEnumerable<string> changedLines = [];
+	private int lineStart = 0;
+
+	private void invokeChange()
+	{
+		lineStart = 0;
+		changedLines = onChange(inputBuffer.ToString());
+	}
+
+	public string ReadInput()
+	{
+		while (true)
+		{
+			string readInput = inputBuffer.ToString();
+
+			Console.Clear();
+			Console.WriteLine($"{prompt}{readInput}");
+
+			foreach (string line in changedLines.Skip(lineStart).Take(10))
+			{
+				Console.WriteLine(line);
+			}
+
+			Console.SetCursorPosition(prompt.Length + cursorLeft, 0);
+
+			ConsoleKeyInfo key = Console.ReadKey(true);
+
+			if (key.Key == ConsoleKey.Enter)
+			{
+				Console.Clear();
+				return readInput;
+			}
+
+			if (key.Key == ConsoleKey.Backspace)
+			{
+				if (cursorLeft > 0)
+				{
+					inputBuffer.Remove(--cursorLeft, 1);
+					invokeChange();
+				}
+
+				continue;
+			}
+
+			if (key.Key == ConsoleKey.Delete)
+			{
+				if (cursorLeft < inputBuffer.Length)
+				{
+					inputBuffer.Remove(cursorLeft, 1);
+					invokeChange();
+				}
+
+				continue;
+			}
+
+			if (key.Key == ConsoleKey.LeftArrow)
+			{
+				if (cursorLeft > 0)
+				{
+					cursorLeft--;
+				}
+
+				continue;
+			}
+
+			if (key.Key == ConsoleKey.RightArrow)
+			{
+				if (cursorLeft < inputBuffer.Length)
+				{
+					cursorLeft++;
+				}
+
+				continue;
+			}
+
+			if (key.Key == ConsoleKey.DownArrow)
+			{
+				if (lineStart + max_display_lines < changedLines.Count())
+				{
+					lineStart++;
+				}
+
+				continue;
+			}
+
+			if (key.Key == ConsoleKey.UpArrow)
+			{
+				if (lineStart > 0)
+				{
+					lineStart--;
+				}
+
+				continue;
+			}
+
+			if (char.IsAscii(key.KeyChar))
+			{
+				inputBuffer.Insert(cursorLeft++, key.KeyChar);
+				invokeChange();
+			}
+		}
+	}
+}
---
+++ TagStorage.Terminal/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TagStorage.Terminal;
+
+public static class Program
+{
+	private static IEnumerable<string> onChange(string input)
+	{
+		return Enumerable.Range(1, input.Length).Select(n => $"{n}{input}");
+	}
+
+	public static void Main(string[] args)
+	{
+		InteractiveInput interactiveInput = new InteractiveInput(onChange, "Enter input: ");
+		string input = interactiveInput.ReadInput();
+		Console.WriteLine($"You entered: {input}");
+	}
+}
